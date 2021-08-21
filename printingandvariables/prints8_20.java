8c6caf2cb7d3a43ba3dcaf1fc2f16f8ca92d117a
akulsharma1 <akulsharma02@gmail.com>
2021-08-20 19:35:54 -0700

    finished first assignment

---
+++ printingandvariables/prints8_20.java
@@ -0,0 +1,18 @@
+package printingandvariables;
+
+
+public class prints8_20 {
+    public static void main(String[] args) {
+        String name = "akul";
+        int age = 16;
+        String hobby = "code";
+        String annoyance = "my parents";
+        String job = "software engineer";
+
+        System.out.println("Hello!\nMy name is "+name+".");
+        System.out.println("I am "+age+" years old.");
+        System.out.println("I love to "+hobby+".");
+        System.out.println(annoyance+" annoy me hella!");
+        System.out.println("I plan to be a "+job+" when I grow up!");
+    }
+}
