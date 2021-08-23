5a1f2797f52d04766869ecffd949d98ad09fa1d4
AKUL SHARMA <akulsharma02@gmail.com>
2021-08-23 12:17:42 -0700

    did rocket.java

---
+++ rocket/rocket.java
@@ -0,0 +1,18 @@
+package rocket;
+
+public class rocket {
+    public static void main(String[] args) {
+        System.out.println("     /\\");
+        System.out.println("    /  \\");
+        System.out.println("   /    \\");
+        System.out.println("  |      |");
+        System.out.println("  | \"lit\"|");
+        System.out.println("  |      |");
+        System.out.println("  |      |");
+        System.out.println(" /        \\");
+        System.out.println("/__________\\");
+        System.out.println("  ||    ||");
+        System.out.println("  ||    ||");
+        System.out.println("  ||    ||");
+    }
+}
