4dfc80fb4fc49e03afb01e8e6080423c97766703
AKUL SHARMA <akulsharma02@gmail.com>
2021-09-08 12:33:18 -0700

    idk how to do this

---
+++ quotientwithoutdivision/quotientwithoutdivision.java
@@ -0,0 +1,13 @@
+package quotientwithoutdivision;
+
+public class quotientwithoutdivision {
+    public static int ReturnQuotient(int num1, int num2) {
+        for (int i = 1; i<=num1; i++) {
+            
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
