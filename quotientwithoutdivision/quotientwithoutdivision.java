a1e91730887ab067ad9e632697b0527ada40f6fa
AKUL SHARMA <akulsharma02@gmail.com>
2021-09-09 12:31:27 -0700

    idk how to finish this lol

--- quotientwithoutdivision/quotientwithoutdivision.java
+++ quotientwithoutdivision/quotientwithoutdivision.java
@@ -1,13 +1,42 @@
 package quotientwithoutdivision;
 
 public class quotientwithoutdivision {
-    public static int ReturnQuotient(int num1, int num2) {
-        for (int i = 1; i<=num1; i++) {
-            
+    public static String ReturnQuotient(int num1, int num2) {
+        System.out.println("got here");
+        int quotient = 0;
+        int remainder = 0;
+        int number1 = num1;
+        while(number1 >= 0) {
+            if (number1 - num2 < 0) {
+                number1=-1;
+            } else {
+                number1 -= num2;
+                quotient++;
+            }
         }
-        return 0;
+
+        //number1second
+        String returnval = quotient + ".";
+        System.out.println(returnval);
+        for(int i = 0; i<10; i++) {
+
+        }
+        // double decquotient = 0;
+        // for(int i = 1; i<10; i++) {
+        //     while (remainder >= 0) {
+        //         if(remainder - num2 < 0) {
+        //             //return quotient+(decquotient*0.1);
+        //         } else {
+        //             remainder -= num2;
+        //             decquotient++;
+        //         }
+        //     }
+        // }
+        
+        return "";
     }
     public static void main(String[] args) {
-        
+        String num = ReturnQuotient(10, 3);
+        System.out.println(num);
     }
 }
