454d61bca7384dd4a50ba5ab70fd05876867bc32
AKUL SHARMA <akulsharma02@gmail.com>
2021-09-16 13:56:48 -0700

    old version (its bad)

--- lancestrece/lancestrece.java
+++ lancestrece/lancestrece.java
@@ -1,40 +1,23 @@
 package lancestrece;
 
+import java.util.*;
 public class lancestrece {
     public static void main(String[] args) {
-        //System.out.println(333/10);
-        CheckTriples(544);
-    }
-    public static void CheckTriples(int num) {
-        int multiplier = 1;
-        int checknum = 0;
-        int onesdigit = 0;
-        int counter = 0;
-        while (true) {
-            if (counter < 3) {
-                onesdigit = num%10;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter an int");
+        int num = s.nextInt();
+        s.nextLine();
+        while(num!=0) {
+            int a = num%10;
+            int b = num%100;
+            int c = num%1000;
+            if(a == b && b == c) {
+                System.out.println("There are 3 numbers in a row");
+                return;
+            }else {
                 num/=10;
-                checknum += multiplier*onesdigit;
-                multiplier *= 10;
-                counter++;
-                System.out.println(checknum);
-                if (counter == 3) {
-                    System.out.println(CheckIfTriple(checknum));
-                }
-            }
-            else {
-                System.out.println(checknum/10);
-                //System.out.println(multiplier);
-                break;
             }
         }
-        //int moddednum = num%10;
-
-    }
-    public static boolean CheckIfTriple(int num) {
-        if (num == 111 || num == 222 || num == 333 || num == 444 || num == 555 || num == 666 || num==777 || num==888 || num==999) {
-            return true;
-        }
-        return false;
+        System.out.println("There are not 3 numbers in a row");
     }
 }
