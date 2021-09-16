0f5d0bb681d9379a89ebff08d6ec84b201fca6e4
AKUL SHARMA <akulsharma02@gmail.com>
2021-09-16 14:10:35 -0700

    did evil6

--- evil6/evil6.java
+++ evil6/evil6.java
@@ -2,6 +2,16 @@ package evil6;
 
 public class evil6 {
     public static void main(String[] args) {
-        
+        ReturnWithoutSix(1006162);
+    }
+    public static void ReturnWithoutSix(int num) {
+        String s = "";
+        while(num > 0) {
+            if(num % 10 != 6) {
+                s = num%10 + s;
+            }
+            num /= 10;
+        }
+        System.out.println(s);
     }
 }
