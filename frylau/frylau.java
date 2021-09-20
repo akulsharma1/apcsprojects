59f176e19225cf59cfdb17bd5afa6ecc1df341e2
AKUL SHARMA <akulsharma02@gmail.com>
2021-09-20 12:11:37 -0700

    did mean

---
+++ frylau/frylau.java
@@ -0,0 +1,13 @@
+package frylau;
+
+public class frylau {
+    public static void mean(int int1, int int2, int int3, int int4, int int5, int int6, int int7) {
+        System.out.println(((double)int1+int2+int3+int4+int5+int6+int7)/7);
+    }
+    public static void median(int int1, int int2, int int3, int int4, int int5, int int6, int int7) {
+        
+    }
+    public static void main(String[] args) {
+        mean(1, 3, 4, 6, 2, 3, 7);
+    }
+}
