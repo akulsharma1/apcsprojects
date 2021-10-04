0bae39a00bda53452d1ec7c2e98bbd0aaa6f516c
AKUL SHARMA <akulsharma02@gmail.com>
2021-10-04 11:59:54 -0700

    added test prog

---
+++ testprogs/testprog1.java
@@ -0,0 +1,9 @@
+package testprogs;
+
+public class testprog1 {
+    public static void main(String[] args) {
+        int x = 3;
+        System.out.println(x++ + ++x);
+        System.out.println();
+    }
+}
