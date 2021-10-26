46d4a89c8461eb96efacd453a00c85961dc79aab
AKUL SHARMA <akulsharma02@gmail.com>
2021-10-26 09:51:21 -0700

    finished check winner

--- connect4/connect4.java
+++ connect4/connect4.java
@@ -15,9 +15,8 @@ public class connect4 {
             for (int y = 0; y < 7; y++) {
                 System.out.print(board[x][y]+"    ");
             }
-            System.out.println();
+            System.out.println("\n");
         }
-        System.out.println();
     }
 
     public static void yellowAddPiece(int column) {
@@ -38,16 +37,51 @@ public class connect4 {
         }
     }
     
-    public static boolean checkWinner() {
-        for (int x = 0; x < 6; x++) {
-            for (int y = 0; y < 7; y++) {
-                //if (board[x][y].equals(board[x+1][y]))
+    public static boolean checkWinner(String playerChar) {
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board[0].length - 3; y++) {
+                if (board[x][y] == playerChar && board[x][y+1] == playerChar && board[x][y+2] == playerChar && board[x][y+3] == playerChar)
+                    return true;
+            }
+        }
+
+        for (int x = 0; x < board.length - 3; x++) {
+            for (int y = 0; y < board[0].length; y++) {
+                if(board[x][y] == playerChar && board[x+1][y] == playerChar && board[x+2][y] == playerChar && board[x+2][y] == playerChar)
+                    return true;
+            }
+        }
+
+        for (int x = 3; x < board.length; x++) {
+            for (int y = 0; y < board[0].length-3; y++) {
+                if(board[x][y] == playerChar && board[x-1][y+1] == playerChar && board[x-2][y+2] == playerChar && board[x-3][y+3] == playerChar)
+                    return true;
+            }
+        }
+
+        for (int x = 0; x < board.length - 3; x++) {
+            for (int y = 0; y < board[0].length - 3; y++) {
+                if (board[x][y] == playerChar && board[x+1][y+1] == playerChar && board[x+2][y+2] == playerChar && board[x+3][y+3] == playerChar)
+                    return true;
             }
         }
         return false;
     }
     public static void main(String[] args) {
         setBoard();
+        //printBoard();
+        blueAddPiece(0);
+        yellowAddPiece(1);
+        blueAddPiece(2);
+        blueAddPiece(3);
+        blueAddPiece(1);
+        blueAddPiece(2);
+        blueAddPiece(3);
+        blueAddPiece(2);
+        blueAddPiece(3);
+        blueAddPiece(3);
         printBoard();
+        System.out.println(checkWinner("B"));
     }
 }
+
