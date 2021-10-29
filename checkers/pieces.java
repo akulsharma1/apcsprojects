4fbe2a523309eb642d4e3131fe131784598fe03f
AKUL SHARMA <akulsharma02@gmail.com>
2021-10-29 11:45:04 -0700

    did pieces.java

---
+++ checkers/pieces.java
@@ -0,0 +1,53 @@
+package checkers;
+
+public class pieces {
+    private int xCor;
+    private int yCor;
+    private boolean king;
+    private String player;
+  
+    public pieces() {
+        xCor = 0;
+        yCor = 0;
+        king = false;
+        player = "1";
+    }
+  
+    public pieces( int x, int y, boolean b, String p) {
+        xCor = x;
+        yCor = y;
+        king = b;
+        player = p;
+    }
+  
+    public int getX() {
+        return xCor;
+    }
+  
+    public int getY() {
+        return yCor;
+    }
+  
+    public boolean kingStatus() {
+        return king;
+    }
+  
+    public void setX(int x) {
+        xCor = x;
+    }
+  
+    public void setY(int y) {
+        yCor = y;
+    }
+  
+    public void makeKing(boolean k) {
+        king = k;
+    }
+
+    public void makePlayer(String s) {
+        player = s;
+    }
+    public String getPlayer() {
+        return player;
+    }
+}
