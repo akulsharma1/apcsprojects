d68fc658d311c8d14e0b309580a81dfe466c099f
AKUL SHARMA <akulsharma02@gmail.com>
2021-11-04 12:27:11 -0700

    Finished p2 no king (has bug)

--- checkers/checkers.java
+++ checkers/checkers.java
@@ -9,16 +9,29 @@ public class checkers {
     public static int counter = 0;
 
     public static void printBoard() {
-        System.out.println("  0 1 2 3 4 5 6 7");
+        String ANSI_RED = "\u001B[31m";
+        String ANSI_RESET = "\u001B[0m";
+        String ANSI_BLUE = "\u001B[34m";
+        System.out.println("    0   1   2   3   4   5   6   7\n");
         for(int y = 0; y < board.length; y++) {
-            System.out.print(y+" ");
+            System.out.print(y+"   ");
             for (int x = 0; x < board[0].length; x++) {
-                if (board[x][y] != null)
-                System.out.print(board[x][y].getPlayer()+" ");
+                if (board[x][y] != null) {
+                    if (board[x][y].getPlayer().equals("1"))
+                        System.out.print(ANSI_RED+board[x][y].getPlayer()+"   "+ANSI_RESET);
+                    else if (board[x][y].getPlayer().equals("1K"))
+                        System.out.print(ANSI_RED+board[x][y].getPlayer()+"  "+ANSI_RESET);
+                    else if (board[x][y].getPlayer().equals("2"))
+                        System.out.print(ANSI_BLUE+board[x][y].getPlayer()+"   "+ANSI_RESET);
+                    else
+                        System.out.print(ANSI_BLUE+board[x][y].getPlayer()+"  "+ANSI_RESET);
+                }
+                
                 else
-                System.out.print("X ");
+                System.out.print("X   ");
+
             }
-            System.out.println();
+            System.out.println("\n");
         }
     }
 
@@ -57,9 +70,9 @@ public class checkers {
         }
     }
     public static boolean checkLegalMove(pieces piece, int endPosx, int endPosy) {
+        if (endPosx > 7 || endPosy > 7 || endPosx < 0 || endPosy < 0) // checks if out of bounds
+            return false;
         if (piece.getPlayer().contains("1")) { // checks if player 1
-            if (endPosx > 7 || endPosy > 7 || endPosx < 0 || endPosy < 0) // checks if out of bounds
-                return false;
             if (!piece.kingStatus()) { // checks if king or not (rules based on kingship)
                 if (piece.getY()-endPosy <= 0) // checks if user's y has moved backwards (if it is <= 0, that means invalid move)
                     return false;
@@ -175,9 +188,82 @@ public class checkers {
                 else return false;
                 return true;
             }
+        } else {
+            if (!piece.kingStatus()) {
+                if (piece.getY()-endPosy >= 0) // checks if user's y has moved backwards (if it is <= 0, that means invalid move)
+                    return false;
+                else if (piece.getX()==endPosx) // checks if user's endposx has not changed (if it hasn't changed = invalid move)
+                    return false;
+                // code for diagonal movements that are not captures
+                // checks if endpos = downwards right diagonal or endpos = downwards left diagonal
+                else if ((piece.getX()+1 == endPosx && piece.getY()+1 == endPosy) || (piece.getX()-1 == endPosx && piece.getY()+1 == endPosy)) {
+                    for (int i = 0; i < p1pieces.length; i++) {
+                        
+                        // checks if there is a player 1 piece already in that spot
+                        if (p1pieces[i].getX() == endPosx && p1pieces[i].getY() == endPosy && p1pieces[i] != piece)
+                            return false;
+                        
+                        // checks if there is a player 2 piece already in that spot
+                        if (p2pieces[i].getX() == endPosx && p2pieces[i].getY() == endPosy)
+                            return false;
+                        
+                    }
+                }
+
+                // code for diagonal captures
+                // checks if move is downwards right capture
+                else if (piece.getX()+2 == endPosx && piece.getY()+2 == endPosy) {
+                    boolean pieceExistsInSpace = false;
+                    for (int i = 0; i < p2pieces.length; i++) {
+                        // checks if user is trying to capture their own piece
+                        if (p1pieces[i].getX() == piece.getX()+1 && p1pieces[i].getY() == endPosy+1)
+                            return false;
+                        // if user is capturing an enemy piece, sets enemy captured piece to null
+                        if (p2pieces[i].getX() == piece.getX()+1 && p2pieces[i].getY() == endPosy+1) {
+                            pieceExistsInSpace = true;
+                            p2pieces[i] = null;
+                        }
+                        if (!pieceExistsInSpace) {
+                            return false;
+                        }
+                    }
+                }
+
+                // checks if move is downwards left capture
+                else if (piece.getX()-2 == endPosx && piece.getY()+2 == endPosy) {
+                    boolean pieceExistsInSpace = false;
+                    for (int i = 0; i < p2pieces.length; i++) {
+                        // checks if user is tryng to capture their own piece
+                        if (p1pieces[i].getX() == piece.getX() - 1 && p1pieces[i].getY() == piece.getY()-1) {
+                            return false;
+                        }
+
+                        // if user is capturing an enemy piece, sets enemy captured piece to null
+                        if (p2pieces[i].getX() == piece.getX() - 1 && p2pieces[i].getY() == piece.getY()-1) {
+                            p2pieces[i] = null;
+                            pieceExistsInSpace=true;
+                        }
+                        if (!pieceExistsInSpace) {
+                            return false;
+                        }
+                    }
+                }
+                else return false;
+                return true;
+            }
         }
         
         return false;
+    }
+
+    public static void kingPiece(pieces piece, int endposx, int endposy) {
+        if (piece.getPlayer().contains("1") && endposy == 0) {
+            piece.makeKing(true);
+            piece.setPlayer("1K");
+        } else if (piece.getPlayer().contains("2") && endposy == 7) {
+            piece.makeKing(true);
+            piece.setPlayer("2K");
+        }
     }
 
     public static int checkWinner() {
@@ -194,7 +280,7 @@ public class checkers {
         else if (!p2piecesExist)
             return 1;
         
-        return -1;
+        return -1; 
     }
     public static void movep1() {
         boolean p1validMove = false;
@@ -223,6 +309,7 @@ public class checkers {
             if (valid) {
                 move(p1pieces[piecenum], xCoor, yCoor);
                 System.out.println("Moved piece");
+                kingPiece(p1pieces[piecenum], xCoor, yCoor);
                 counter++;
             }
             int winner = checkWinner();
@@ -263,6 +350,7 @@ public class checkers {
             if (legal) {
                 move(p2pieces[piecenum], xCoor, yCoor);
                 System.out.println("Moved piece");
+                kingPiece(p2pieces[piecenum], xCoor, yCoor);
                 counter++;
             } else {
                 System.out.println("Illegal move");
@@ -287,7 +375,12 @@ public class checkers {
         createp1Pieces();
         createp2Pieces();
         makeBoard();
-        printBoard();
-        
+        while (counter <= 64) {
+            movep1();
+            movep2();
+        }
+        if (counter > 64) {
+            System.out.println("Tie");
+        }
     }
 }
