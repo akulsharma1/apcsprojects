e1975277914a2b479ef49d776bb8d72025cc5a52
AKUL SHARMA <akulsharma02@gmail.com>
2021-11-04 11:02:38 -0700

    did more checkers

--- checkers/checkers.java
+++ checkers/checkers.java
@@ -6,6 +6,7 @@ public class checkers {
     public static pieces[] p1pieces = new pieces[12];
     public static pieces[] p2pieces = new pieces[12];
     public static pieces[][] board = new pieces[8][8];
+    public static int counter = 0;
 
     public static void printBoard() {
         System.out.println("  0 1 2 3 4 5 6 7");
@@ -56,103 +57,237 @@ public class checkers {
         }
     }
     public static boolean checkLegalMove(pieces piece, int endPosx, int endPosy) {
-        if (piece.getPlayer().contains("1")) {
-            if (endPosx > 7 || endPosy > 7 || endPosx < 0 || endPosy < 0)
+        if (piece.getPlayer().contains("1")) { // checks if player 1
+            if (endPosx > 7 || endPosy > 7 || endPosx < 0 || endPosy < 0) // checks if out of bounds
                 return false;
-            if (!piece.kingStatus()) {
-                if (piece.getY()-endPosy <= 0)
+            if (!piece.kingStatus()) { // checks if king or not (rules based on kingship)
+                if (piece.getY()-endPosy <= 0) // checks if user's y has moved backwards (if it is <= 0, that means invalid move)
                     return false;
-                if (piece.getX()==endPosx)
+                else if (piece.getX()==endPosx) // checks if user's x has not changed (if it hasn't changed, that means invalid move)
                     return false;
-                if ((piece.getX()+1 == endPosx && piece.getY()-1 == endPosy) || (piece.getX()-1 == endPosx && piece.getY()-1 == endPosy)) {
+                    
+                // code for diagonal movements that are not captures
+                // checks if endpos = upwards right diagonal or endpos = upwards left diagonal
+                else if ((piece.getX()+1 == endPosx && piece.getY()-1 == endPosy) || (piece.getX()-1 == endPosx && piece.getY()-1 == endPosy)) {
                     for (int i = 0; i < p1pieces.length; i++) {
-                        if (p1pieces[i].getX() == endPosx && p1pieces[i].getY() == endPosy)
+                        
+                        // checks if there is a player 1 piece already in that spot
+                        if (p1pieces[i].getX() == endPosx && p1pieces[i].getY() == endPosy && p1pieces[i] != piece)
                             return false;
+                        
+                        // checks if there is a player 2 piece already in that spot
                         if (p2pieces[i].getX() == endPosx && p2pieces[i].getY() == endPosy)
                             return false;
+                        
                     }
                 }
 
-                if (piece.getX()+2 == endPosx && piece.getY()-2 == endPosy) {
+                // code for diagonal captures
+                // checks if move is upwards right capture
+                else if (piece.getX()+2 == endPosx && piece.getY()-2 == endPosy) {
+                    boolean pieceExistsInSpace = false;
                     for (int i = 0; i < p2pieces.length; i++) {
-                        if (p1pieces[i].getX() == endPosx - 1 && p1pieces[i].getY() == endPosy+1) {
+                        // checks if user is trying to capture their own piece
+                        if (p1pieces[i].getX() == endPosx - 1 && p1pieces[i].getY() == endPosy+1)
                             return false;
-                        }
+                        // if user is capturing an enemy piece, sets enemy captured piece to null
                         if (p2pieces[i].getX() == endPosx - 1 && p2pieces[i].getY() == endPosy+1) {
+                            pieceExistsInSpace = true;
                             p2pieces[i] = null;
+                        }
+                        if (!pieceExistsInSpace) {
+                            return false;
                         }
                     }
                 }
-                if (piece.getX()-2 == endPosx && piece.getY()-2 == endPosy) {
+
+                // checks if move is upwards left capture
+                else if (piece.getX()-2 == endPosx && piece.getY()-2 == endPosy) {
                     for (int i = 0; i < p2pieces.length; i++) {
+                        // checks if user is tryng to capture their own piece
                         if (p1pieces[i].getX() == endPosx + 1 && p1pieces[i].getY() == endPosy+1) {
                             return false;
                         }
+
+                        // if user is capturing an enemy piece, sets enemy captured piece to null
                         if (p2pieces[i].getX() == endPosx + 1 && p2pieces[i].getY() == endPosy+1) {
                             p2pieces[i] = null;
                         }
                     }
                 }
-
+                else return false;
                 return true;
+            } else { // code for player 1 king
+                if (piece.getY() == endPosy) // checks if user's endposy has not changed (if it hasn't changed = invalid move)
+                    return false;
+                else if (piece.getX()==endPosx) // checks if user's endposx has not changed (if it hasn't changed = invalid move)
+                    return false;
 
-            } else {
-                if (piece.getY()-endPosy == 0)
-                    return false;
-                if (piece.getX()==endPosx)
-                    return false;
-                if (piece.getX()+1 == endPosx) {
-                    if (piece.getY()-1 == endPosy) {
-                        for (int i = 0; i < p1pieces.length; i++) {
-                            if (p1pieces[i].getX() == endPosx && p1pieces[i].getY() == endPosy && p1pieces[i] != piece)
-                                return false;
-                            if (p2pieces[i].getX() == endPosx && p2pieces[i].getY() == endPosy)
-                                return false;
-                        }
-                    } else if (piece.getY()+1 == endPosy) {
-                        for (int i = 0; i < p1pieces.length; i++) {
-                            if (p1pieces[i].getX()+1 == endPosx && p1pieces[i].getY() +1 == endPosy && p1pieces[i] != piece)
-                                return false;
-                            if (p2pieces[i].getX()+1== endPosx && p2pieces[i].getY()+1 == endPosy)
-                                return false;
-                        }
+                // checks all diagonals for kings
+                else if ((piece.getX()-1 == endPosx && piece.getY() - 1 == endPosy) || (piece.getX()-1 == endPosx && piece.getY() + 1 == endPosy) || (piece.getX()+1 == endPosx && piece.getY() - 1 == endPosy) || (piece.getX()+1 == endPosx && piece.getY() + 1 == endPosy)) {
+                    for (int i = 0; i < p2pieces.length; i++) {
+                        // checks if a piece (either player 1 or player 2 already exists in given spot)
+                        if (p1pieces[i].getX() == endPosx && p1pieces[i].getY() == endPosy) 
+                            return false;
+                        if (p2pieces[i].getX() == endPosx && p1pieces[i].getY() == endPosy)
+                            return false;
+                    }
+                }
+
+                // checks upwards left capturing diagonal
+                else if (piece.getX()-2 == endPosx && piece.getY()-2 == endPosy) {
+                    for (int i = 0; i < p2pieces.length; i++) {
+                        if (p1pieces[i].getX() == piece.getX()-1 && p1pieces[i].getY() == piece.getY()-1)
+                            return false;
+                        if (p2pieces[i].getX() == piece.getX()-1 && p2pieces[i].getY() == piece.getY()-1)
+                            p2pieces[i] = null;
+                    }
+                }
+
+                // checks downwards left capturing diagonal
+                else if (piece.getX()-2 == endPosx && piece.getY()+2 == endPosy) {
+                    for (int i = 0; i < p2pieces.length; i++) {
+                        if (p1pieces[i].getX() == piece.getX()-1 && p1pieces[i].getY() == piece.getY()+1)
+                            return false;
+                        if (p2pieces[i].getX() == piece.getX()-1 && p2pieces[i].getY() == piece.getY()+1)
+                            p2pieces[i] = null;
+                    }
+                }
+                // checks upwards right capturing diagonal
+                else if (piece.getX()+2 == endPosx && piece.getY()-2 == endPosy) {
+                    for (int i = 0; i < p2pieces.length; i++) {
+                        if (p1pieces[i].getX() == piece.getX()+1 && p1pieces[i].getY() == piece.getY()-1)
+                            return false;
+                        if (p2pieces[i].getX() == piece.getX()+1 && p2pieces[i].getY() == piece.getY()-1)
+                            p2pieces[i] = null;
                     }
                 }
                 
+                // checks downwards right capturing diagonal
+                else if (piece.getX()+2 == endPosx && piece.getY()+2 == endPosy) {
+                    for (int i = 0; i < p2pieces.length; i++) {
+                        if (p1pieces[i].getX() == piece.getX()+1 && p1pieces[i].getY() == piece.getY()+1)
+                            return false;
+                        if (p2pieces[i].getX() == piece.getX()+1 && p2pieces[i].getY() == piece.getY()+1)
+                            p2pieces[i] = null;
+                    }
+                }
+                else return false;
+                return true;
             }
         }
         
         return false;
     }
-    public static void movep1() {
-        System.out.println("---------------------");
+
+    public static int checkWinner() {
+        boolean p1piecesExist = false;
+        boolean p2piecesExist = false;
         for (int i = 0; i < p1pieces.length; i++) {
-            if (p1pieces[i].getX() >= 0)
-                System.out.println("Piece "+i+":   x:"+p1pieces[i].getX()+"    y:"+p1pieces[i].getY());
+            if (p1pieces[i] != null)
+                p1piecesExist = true;
+            if (p1pieces[i] != null)
+                p2piecesExist = true;
         }
-        System.out.println("---------------------");
+        if (!p1piecesExist)
+            return 2;
+        else if (!p2piecesExist)
+            return 1;
+        
+        return -1;
+    }
+    public static void movep1() {
+        boolean p1validMove = false;
+        while (!p1validMove) {
+            printBoard();
+            System.out.println("---------------------");
+            for (int i = 0; i < p1pieces.length; i++) {
+                if (p1pieces[i].getX() >= 0)
+                    System.out.println("Piece "+i+":   x:"+p1pieces[i].getX()+"    y:"+p1pieces[i].getY());
+            }
+            System.out.println("---------------------");
 
-        Scanner s = new Scanner(System.in);
-        System.out.println("Player 1: Enter which piece you would like to move");
-        int piecenum = s.nextInt();
-        s.nextLine();
+            Scanner s = new Scanner(System.in);
+            System.out.println("Player 1: Enter which piece you would like to move");
+            int piecenum = s.nextInt();
+            s.nextLine();
 
-        System.out.println("Player 1: Enter the x coordinate which you would like to move to");
-        int xCoor = s.nextInt();
-        s.nextLine();
-        System.out.println("Player 1: Enter the y coordinate which you would like to move to");
-        int yCoor = s.nextInt();
-        s.nextLine();
+            System.out.println("Player 1: Enter the x coordinate which you would like to move to");
+            int xCoor = s.nextInt();
+            s.nextLine();
+            System.out.println("Player 1: Enter the y coordinate which you would like to move to");
+            int yCoor = s.nextInt();
+            s.nextLine();
+            
+            boolean valid = checkLegalMove(p1pieces[piecenum], xCoor, yCoor);
+            if (valid) {
+                move(p1pieces[piecenum], xCoor, yCoor);
+                System.out.println("Moved piece");
+                counter++;
+            }
+            int winner = checkWinner();
+            if (winner != -1) {
+                System.out.println("Player "+winner+" wins!");
+                System.exit(0);
+            }
+            if (valid) {
+                p1validMove = true;
+            }
+        }
 
-        //if (!king )
+    }
+    public static void movep2() {
+        boolean p2validMove = false;
+        while (!p2validMove) {
+            printBoard();
+            System.out.println("---------------------");
+            for (int i = 0; i < p2pieces.length; i++) {
+                if (p2pieces[i].getX() >= 0)
+                    System.out.println("Piece "+i+":   x:"+p2pieces[i].getX()+"    y:"+p2pieces[i].getY());
+            }
+            System.out.println("---------------------");
 
+            Scanner s = new Scanner(System.in);
+            System.out.println("Player 2: Enter which piece you would like to move");
+            int piecenum = s.nextInt();
+            s.nextLine();
+
+            System.out.println("Player 2: Enter the x coordinate which you would like to move to");
+            int xCoor = s.nextInt();
+            s.nextLine();
+            System.out.println("Player 2: Enter the y coordinate which you would like to move to");
+            int yCoor = s.nextInt();
+            s.nextLine();
+
+            boolean legal = checkLegalMove(p2pieces[piecenum], xCoor, yCoor);
+            if (legal) {
+                move(p2pieces[piecenum], xCoor, yCoor);
+                System.out.println("Moved piece");
+                counter++;
+            } else {
+                System.out.println("Illegal move");
+            }
+            int winner = checkWinner();
+            if (winner != -1) {
+                System.out.println("Player "+winner+" wins!");
+                System.exit(0);
+            }
+            if(legal) {
+                p2validMove = true;
+            }
+        }
+    }
+    public static void move(pieces piece, int xCoor, int yCoor) {
+        board[piece.getX()][piece.getY()] = null;
+        piece.setX(xCoor);
+        piece.setY(yCoor);
+        board[xCoor][yCoor] = piece;
     }
     public static void main(String[] args) {
         createp1Pieces();
         createp2Pieces();
         makeBoard();
         printBoard();
-        movep1();
-        //movep2();
+        
     }
 }
