88b1c5a464ebdab545817c14c439b5aadbe44ce1
AKUL SHARMA <akulsharma02@gmail.com>
2022-01-25 16:51:10 -0800

    did blackjack

--- blackjack/blackjack.java
+++ blackjack/blackjack.java
@@ -1,74 +1,168 @@
 package blackjack;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 public class blackjack {
     public static int dealerScore;
 
-    public static void playRound(String name, int balance) {
-        Scanner input = new Scanner(System.in);
-        final int maxScore = 21;
+    public static boolean dealerGot11Ace = false;
+    public static deck deck = new deck();
 
-        int playerCardCounter = 0;
-        int dealerCardCounter = 0;
-        int deckCounter = 0;
-
+    public static ArrayList<Integer> dealerFirstPlay() {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int deckValue = 0;
+        int dealerScore = 0;
         
-        deck cardDeck = new deck();
-        cardDeck.shuffle();
-        player player = new player(name, balance);
-
-        System.out.println(name+" draws: "+cardDeck.getCardFromIndex(deckCounter).getName()+" of "+cardDeck.getCardFromIndex(deckCounter).getSuit());
-        playerCardCounter += cardDeck.getCardFromIndex(deckCounter).getValue();
-        deckCounter++;
-        System.out.println(name+" draws: "+cardDeck.getCardFromIndex(deckCounter).getName()+" of "+cardDeck.getCardFromIndex(deckCounter).getSuit());
-        playerCardCounter += cardDeck.getCardFromIndex(deckCounter).getValue();
-        deckCounter++;
-        System.out.println("Total value of players cards: "+playerCardCounter);
+        list.add(0);
+        list.add(0); // placeholder
+        card card1 = deck.getCardFromIndex(deckValue);
+        dealerScore += card1.getValue();
+        System.out.println("Dealer draws a " + card1.getName() +" of "+card1.getSuit() + ".");
+        deckValue++;
+        list.add(dealerScore);
 
 
-        System.out.println("Dealer draws: "+cardDeck.getCardFromIndex(deckCounter).getName()+" of "+cardDeck.getCardFromIndex(deckCounter).getSuit());
-        dealerCardCounter += cardDeck.getCardFromIndex(deckCounter).getValue();
-        System.out.println("Dealer card value: "+dealerCardCounter);
-        deckCounter++;
-        dealerCardCounter += cardDeck.getCardFromIndex(deckCounter).getValue();
-        deckCounter++;
-        while (true) {
-            System.out.println("Options: \n1. Hit \n2. Stand");
-            int option = input.nextInt();
-            if (option == 1) {
-                System.out.println(name+" draws: "+cardDeck.getCardFromIndex(deckCounter).getName()+" of "+cardDeck.getCardFromIndex(deckCounter).getSuit());
-                if (cardDeck.getCardFromIndex(deckCounter).getValue() == -1) {
-                    if (playerCardCounter + 11 > maxScore) {
-                        playerCardCounter += 1;
-                    } else {
-                        playerCardCounter += 11;
-                    }
-                    
+        // card card2 = deck.getCardFromIndex(deckValue);
+        // dealerScore += card2.getValue();
+        // System.out.println("Dealer draws a " + card2.getName() +" of "+card2.getSuit() + ".");
+        // deckValue++;
+        list.set(0, deckValue);
+
+        return list;
+    }
+    public static ArrayList<Integer> dealerSecondPlay(player player, ArrayList<Integer> details) {
+        int deckValue = details.get(0);
+        int dealerScore = details.get(2);
+
+        card card1 = deck.getCardFromIndex(deckValue);
+        dealerScore += card1.getValue();
+        System.out.println("Dealer's hidden card was a " + card1.getName() +" of "+card1.getSuit() + ".");
+        deckValue++;
+
+        while (dealerScore <= 17) {
+            card card = deck.getCardFromIndex(deckValue);
+            System.out.println("Dealer draws a " + card.getName() +" of "+card.getSuit() + ".");
+            if (card.getValue() == 11) {
+                if (dealerScore + 11 > 21) {
+                    dealerScore += 1;
                 } else {
-                    playerCardCounter += cardDeck.getCardFromIndex(deckCounter).getValue();
+                    dealerScore += 11;
                 }
-                deckCounter++;
+            } else {
+                dealerScore += card.getValue();
+            }
+            
+            deckValue++;
+        }
+        details.add(dealerScore);
+        details.set(0, deckValue);
 
-                if (playerCardCounter > maxScore) {
-                    System.out.println(name+" Bust!");
-                    break;
-                }
-            } else if (option == 2) {
-                while (true) {
-                    System.out.println("Dealer draws: "+cardDeck.getCardFromIndex(deckCounter).getName()+" of "+cardDeck.getCardFromIndex(deckCounter).getSuit());
-                    
-                    if (dealerCardCounter == 17) {
+        return details;
+    }
+    public static ArrayList<Integer> playerPlays(player player, ArrayList<Integer> details) {
+        Scanner input = new Scanner(System.in);
+        int playerScore = 0;
+        int deckValue = details.get(0);
+        // details(0) = current index of deck
+        // details(1) = current score of player 
+        // details(2) = 1 hit score of dealer
+        // details(3) == 2 hit score of dealer
+        
 
-                    } else if (dealerCardCounter < 17) {
+        // play 1
+        card card1 = deck.getCardFromIndex(deckValue);
+        playerScore += card1.getValue();
+        System.out.println(player.getName() +" Draws a " + card1.getName() +" of "+card1.getSuit() + ".");
+        deckValue++;
 
-                    } else if (dealerCardCounter > 21){
-                        System.out.println("Dealer bust!");
-                        break;
+        // play 2
+        card card2 = deck.getCardFromIndex(deckValue);
+        playerScore += card2.getValue();
+        System.out.println(player.getName() +" Draws a " + card2.getName() +" of "+card2.getSuit() + ".");
+        
+        deckValue++;
+
+        System.out.println("Dealer score is: " + details.get(2));
+        // play until player busts or chooses to stay
+        while (true) {
+            System.out.println("Your current score is " + playerScore + ".");
+            System.out.println("Press 1 to hit. Press 2 to stand");
+            int playerChoice = input.nextInt();
+            input.nextLine();
+            if (playerChoice == 1) {
+                
+                card card = deck.getCardFromIndex(deckValue);
+                System.out.println(player.getName()+" draws a " + card.getName() +" of "+card.getSuit() + ".");
+                if (card.getValue() == 11) {
+                    if (playerScore + 11 > 21) {
+                        playerScore++;
+                    } else {
+                        playerScore += 11;
                     }
+                } else {
+                    playerScore += card.getValue();
                 }
+                deckValue++;
+                if (playerScore > 21) {
+                    int dealerOnePlay = details.get(2);
+                    details.set(0, deckValue);
+                    details.set(1, playerScore);
+                    details.set(2, dealerOnePlay);
+                    return details;
+                }
+            } else {
+                int dealerOnePlay = details.get(2);
+                details.set(0, deckValue);
+                details.set(1, playerScore);
+                details.set(2, dealerOnePlay);
+                return details;
             }
         }
+    }
 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = input.nextLine();
+        player player = new player(name, 500);
+        while (player.getBankroll() > 0) {
+            System.out.println("Your balance is "+ player.getBankroll()+". Press 1 to Play. Press 2 to Quit.");
+            int playerChoice = input.nextInt();
+            input.nextLine();
 
+            if (playerChoice != 1) {
+                break;
+            }
+            System.out.println("Enter bet");
+            int bet = input.nextInt();
+            input.nextLine();
+
+            deck.shuffle();
+            ArrayList<Integer> details = dealerFirstPlay();
+            details = playerPlays(player, details);
+            details = dealerSecondPlay(player, details);
+            if (details.get(1) > 21) {
+                System.out.println(player.getName()+" busted!");
+                player.setBankroll(player.getBankroll()-bet);
+                continue;
+            } else if (details.get(3) > 21) {
+                System.out.println(details.get(3));
+                System.out.println("Dealer busted!");
+                player.setBankroll(player.getBankroll()+(int)(bet * 1.5));
+                continue;
+            }
+            else if (details.get(1) > details.get(3) && details.get(1) <= 21) {
+                System.out.println(player.getName()+" wins!");
+                player.setBankroll(player.getBankroll()+(int)(bet * 1.5));
+                continue;
+            } else if (details.get(1) < details.get(3) && details.get(3) <= 21) {
+                System.out.println("Dealer wins!");
+                player.setBankroll(player.getBankroll()-bet);
+                continue;
+            } else {
+                System.out.println("Tie!");
+                continue;
+            }
+        }
     }
 }
--- blackjack/deck.java
+++ blackjack/deck.java
@@ -2,10 +2,10 @@ package blackjack;
 
 import java.util.ArrayList;
 public class deck {
-    private static ArrayList<card> cards;
+    private static ArrayList<card> cards = new ArrayList<card>();
 
     public deck() {
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 2; i <= 10; i++) {
             cards.add(new card(Integer.toString(i), "spades", i));
             cards.add(new card(Integer.toString(i), "hearts", i));
             cards.add(new card(Integer.toString(i), "clubs", i));
@@ -27,10 +27,10 @@ public class deck {
         cards.add(new card("King", "clubs", 10));
         cards.add(new card("King", "diamonds", 10));
 
-        cards.add(new card("Ace", "spades", -1));
-        cards.add(new card("Ace", "hearts", -1));
-        cards.add(new card("Ace", "clubs", -1));
-        cards.add(new card("Ace", "diamonds", -1));
+        cards.add(new card("Ace", "spades", 11));
+        cards.add(new card("Ace", "hearts", 11));
+        cards.add(new card("Ace", "clubs", 11));
+        cards.add(new card("Ace", "diamonds", 11));
     }
 
     public void shuffle() {
