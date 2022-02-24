0d00b498469c5560e5da0a730f2369a765263d3b
akulsharma1 <akulsharma02@gmail.com>
2022-02-23 19:16:53 -0800

    did all of mowing

--- mowing/mowing.java
+++ mowing/mowing.java
@@ -2,13 +2,15 @@ package mowing;
 
 import java.util.*;
 public class mowing {
-    public static String[][] lawnArray;
+    public static lawn[] lawnList;
+
     public static void acceptInputs() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the number of lawns");
         int lawns = input.nextInt();
+        lawnList = new lawn[lawns];
+
         for(int i = 0; i < lawns; i++) {
-            
             System.out.println("Enter length of lawn " + (i + 1));
             int length = input.nextInt();
             input.nextLine();
@@ -17,22 +19,49 @@ public class mowing {
             input.nextLine();
             
 
-            System.out.println("Enter y coordinate of mower");
-            int y = input.nextInt();
-            input.nextLine();
             System.out.println("Enter x coordinate of mower");
             int x = input.nextInt();
             input.nextLine();
+            System.out.println("Enter y coordinate of mower");
+            int y = input.nextInt();
+            input.nextLine();
             
-            lawnArray = new String[length][width];
+            lawn lawn = new lawn(length, width, y, x);
             for (int j = 0; j < length; j++) {
                 System.out.println("Enter the lawn row. For every line put . if it is empty, and t if there is a tree.");
                 String s = input.nextLine();
                 for (int z = 0; z < s.length(); z++) {
-                    lawnArray[i][j] = Character.toString(s.charAt(z));
+                    lawn.setPos(Character.toString(s.charAt(z)), j, z);
                 }
             }
+            lawnList[i] = lawn;
         }
 
+    }
+
+    public static void traverse(lawn lawn) {
+        String path = lawn.checkAvailableMoves(lawn.getStartRow(), lawn.getStartColumn());
+        if (path.equals("up")) {
+            lawn.setStartRow(lawn.getStartRow()-1);
+            traverse(lawn);
+        } else if (path.equals("down")) {
+            lawn.setStartRow(lawn.getStartRow()+1);
+            traverse(lawn);
+        } else if (path.equals("left")) {
+            lawn.setStartColumn(lawn.getStartColumn()-1);
+            traverse(lawn);
+        } else if (path.equals("right")) {
+            lawn.setStartColumn(lawn.getStartColumn()+1);
+            traverse(lawn);
+        } else {
+            lawn.printArray();
+        }
+    }
+
+    public static void main(String[] args) {
+        acceptInputs();
+        for (lawn l : lawnList) {
+            traverse(l);
+        }
     }
 }
