b80dc91c71aafa90931ae5415905b977aece842b
akulsharma1 <akulsharma02@gmail.com>
2022-03-09 17:23:47 -0800

    finished set3

--- recursionproblemset3/set3.java
+++ recursionproblemset3/set3.java
@@ -16,7 +16,30 @@ public class set3 {
             System.out.println(printStars(count));
         }
     }
+
+    public static void printNum(int num, int maxNum) {
+        if (num < maxNum) {
+            System.out.print(num+", ");
+            printNum(num + 1, maxNum);
+        } else if (num == maxNum) {
+            System.out.print(num);
+        }
+    }
+
+    public static void convertToBinary(int num) {
+        if (num < 2) System.out.print(num%2);
+        else {
+            convertToBinary(num / 2);
+            convertToBinary(num % 2);
+        }
+    }
+    
     public static void main(String[] args) {
         doubleMountain(3);
+        printNum(1, 10);
+        System.out.println();
+        convertToBinary(8);
+        
+
     }
 }
