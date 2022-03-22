e3b49fb4dd2416ce482b573d7bd2fd3f3f34d2a3
AKUL SHARMA <akulsharma02@gmail.com>
2022-03-22 12:15:34 -0700

    did set4

---
+++ recursionproblemset4/set4.java
@@ -0,0 +1,57 @@
+package recursionproblemset4;
+
+
+public class set4 {
+
+	public static void main(String[] args) {
+		int [] nums = {2,4,8};
+		System.out.println(groupSum(0, nums, 10));
+		
+		System.out.println(split53(new int[]{2,4,2}));
+		
+		int [] nums3 = {2,2};
+		System.out.println(splitArray(nums3));
+	}
+	
+
+	public static boolean groupSum(int start, int[] nums, int target) {
+		if(target == 0) return true; // if target is 0, no array is needed
+        else if(start == nums.length) return false; // array has been fully iterated through, meaning it's done
+        else if(groupSum(start+1, nums, target - nums[start])) return true; // if the next recursive number adds it up to target, return true
+        return groupSum(start + 1, nums, target); // recursive case continues to iterate through array
+	}
+	
+	//2
+	public static boolean split53(int[] nums) {
+		return split53helper(0,nums,0,0);
+	}
+    // I used a helper function because it's not possible without extra inputs to the recursive function
+	public static boolean split53helper(int i, int[] arr, int group1Sum, int group2Sum) {
+	    if (i == arr.length) 
+            return group1Sum == group2Sum; // base case
+	    else if (arr[i] % 5 != 0 && split53helper(i+1, arr, group1Sum+arr[i], group2Sum))
+            return true;
+	    else if (arr[i] % 3 != 0 && split53helper(i+1, arr, group1Sum, group2Sum+arr[i])) {
+		    return true;
+	    }
+	
+	    return false;
+	}
+		
+		
+	public static boolean splitArray(int[] nums) {
+		return splitArrayHelper(0,nums,0,0);
+	}
+	public static boolean splitArrayHelper(int i, int[] arr, int group1Sum, int group2Sum) {
+		if (i == arr.length) return group1Sum==group2Sum;
+		if (splitArrayHelper(i+1, arr, group1Sum+arr[i], group2Sum))
+		  	return true;
+		  
+		else if (splitArrayHelper(i+1, arr, group1Sum, group2Sum+arr[i])) 
+		  	return true;
+		
+		return false;
+	}
+		
+	
+}
