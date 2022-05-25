c93e4da23ec3409f8e7b021edd919e308afd8a77
AKUL SHARMA <akulsharma02@gmail.com>
2022-05-25 12:28:52 -0700

    added more handlers

--- compiler/compiler.java
+++ compiler/compiler.java
@@ -6,35 +6,55 @@ import java.util.*;
 import java.io.*;  // Import the File class
 
 public class compiler {
+    
+
+
     // splits the given line into an arraylist of all the characters.
     public static ArrayList<String> createLineArr(String s) {
         ArrayList<String> lineArr = new ArrayList<String>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != ' ') {
-                lineArr.add(s.substring(i, i + 1));
-            }
+        String str[] = s.split(" ");
+        for (int i = 0; i < str.length; i++) {
+            lineArr.add(str[i]);
         }
         return lineArr;
     }
 
-    // checks if the given line contains a string. If so, it returns true.
-    public static boolean checkIfListContainsString(ArrayList<String> list, String str) {
-        for (String s : list) {
-            if (s.equals(str)) {
-                return true;
-            }
-        }
-        return false;
+    
+
+
+    // checks what type of line the given line is.
+    public static int checkLineType(ArrayList<String> list) {
+        if (list.get(0).equals("string")) return 1;
+        if (list.get(0).equals("int")) return 2;
+        if (list.get(0).equals("input")) return 3;
+        if (list.get(0).equals("math")) return 4;
+        return -1;
     }
 
-    // checks if the given line uses a math function. If so, it returns the type.
-    public static int checkIfMathFunction(ArrayList<String> list) {
-        if (checkIfListContainsString(list, "+")) return 1;
-        if (checkIfListContainsString(list, "-")) return 2;
-        if (checkIfListContainsString(list, "*")) return 3;
-        if (checkIfListContainsString(list, "/")) return 4;
-        if (checkIfListContainsString(list, "%")) return 5;
-        return -1;
+    public static void variableHandler(ArrayList<String> list) {
+        if (checkLineType(list) == 1) {
+            helpers.addToStringVars(list.get(1), list.get(3));
+        } else if (checkLineType(list) == 2) {
+            helpers.addToIntVars(list.get(1), Integer.parseInt(list.get(3)));
+        }
+    }
+
+    public static void inputHandler() {
+        Scanner s = new Scanner(System.in);
+        String l = s.nextLine();
+        System.out.println(l);
+    }
+
+    // handles everything from the math class.
+    public static void mathHandler(ArrayList<String> list) {
+        int mathType = math.checkMathFunction(list);
+        if (mathType == 1) {
+            try{System.out.println(math.add(list));}
+            catch(Exception e){System.out.println("Error: " + e);}
+        } else if (mathType == 2) {
+            try{System.out.println(math.subtract(list));}
+            catch(Exception e){System.out.println("Error: " + e);}
+        }
     }
     public static void main(String[] args) {
         try {
@@ -43,25 +63,21 @@ public class compiler {
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 ArrayList<String> list = createLineArr(data);
-                int mathFunction = checkIfMathFunction(list);
-                if (mathFunction == 1) {
-                    try{System.out.println(add(list));}
-                    catch(Exception e){System.out.println("Error: " + e);}
+                int type = checkLineType(list);
+
+                if (type == 1 || type == 2) {
+                    variableHandler(list);
+                } else if (type == 3) {
+                    inputHandler();
+                } else if (type == 4) {
+                    mathHandler(list);
                 }
+                
             }
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-    }
-
-
-    public static int add(ArrayList<String> arr) throws Exception {
-        System.out.println(arr);
-        for (int i = 0; i < arr.size(); i++) {
-            System.out.println(arr.get(i));
-        }
-        throw new Exception("Not valid sum");
     }
 }
